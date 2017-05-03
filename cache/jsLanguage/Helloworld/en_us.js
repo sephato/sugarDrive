7cba644e99d94952bf74ddb1d47c4edff1f4e334
root <root@Sephato.etna.local>
2017-05-03 17:22:10 +0200

    ajout d'une ligne

---
+++ cache/jsLanguage/Helloworld/en_us.js
@@ -0,0 +1,1 @@
+SUGAR.language.setLanguage('Helloworld', null);
--- modules/Helloworld/views/view.list.php
+++ modules/Helloworld/views/view.list.php
@@ -3,5 +3,6 @@ class HelloworldViewList extends SugarView{
   public function display()
   {
     echo '<h1>Hello World</h1>';
+    echo 'test1';
   }
 }
